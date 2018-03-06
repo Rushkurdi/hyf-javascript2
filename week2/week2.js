839fe5ac6682c375326e88aa7a97444f1caa72ba
Rashad <rushkurdi@gmail.com>
2018-03-06 16:32:30 +0100

    mapAndFilter

---
+++ week2/week2.js
@@ -0,0 +1,8 @@
+var numbers = [1, 2, 3, 4];
+
+var newNumbers = numbers.filter(function (number) {
+    return (number % 2 !== 0);
+}).map(function (number) {
+    return number * 2;
+});
+console.log("The doubled numbers are", newNumbers);
