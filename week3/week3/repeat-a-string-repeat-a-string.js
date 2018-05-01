8b1edd0c4828e1d25aba35db8d1356e590ca1b47
RushKurdi <Rushkurdi@users.noreply.github.com>
2018-05-01 22:07:04 +0200

    new update

---
+++ week3/week3/repeat-a-string-repeat-a-string.js
@@ -0,0 +1,44 @@
+function repeatStringNumTimes(str, num) {
+
+    var strRepeated = "";
+
+    for (var i = 0; i < num; i++) {
+        strRepeated += str;
+    }
+
+    return strRepeated;
+}
+
+repeatStringNumTimes("abc", 3);
+
+
+function repeatStringNumTimes(str, num) {
+
+    var strRepeated = "";
+    var i = 0;
+    while (i < num) {
+        strRepeated += str;
+        i++;
+    }
+
+    return strRepeated;
+}
+
+
+repeatStringNumTimes("abc", 3);
+
+
+function repeatStringNumTimes(str, num) {
+
+    var strRepeated = "";
+    var i = 0;
+    do {
+        strRepeated += str;
+        i++;
+    }
+    while (i < num);
+    return strRepeated;
+}
+
+
+repeatStringNumTimes("abc", 3);
